440b7fcbed0e17c2d63caeb486371cd1d92176ae
borkmaster <unluckymattress@gmail.com>
2017-11-22 13:31:57 -0600

    Paddle movement, ball mechanics updates
    - Paddle now moves left and right with mouse
    - Ball now stays on top of paddle until game starts
    - Ball launches and game starts upon left click

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+	
+	private Vector3 paddleToBallVector;
+	private bool ballLaunched;
+
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		ballLaunched = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (!ballLaunched) {
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			
+			if (Input.GetMouseButtonDown (0)) {
+				print ("Mouse clicked, launch ball");
+				ballLaunched = true;
+				this.rigidbody2D.velocity = new Vector2 (2f, 10f);
+			}
+		} 
+		
+	}
+}
