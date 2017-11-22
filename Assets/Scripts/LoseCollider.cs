500f07fef71ff6e7099cc3fcaa757b8ba44e4a8d
unluckymattress <unluckymattress@gmail.com>
2017-11-21 22:26:05 -0600

    Added paddle, ball bounce

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+	void OnTriggerEnter2D (Collider2D trigger) {
+		print ("Triggered");
+		levelManager.LoadLevel ("Lose");
+	}
+
+}
