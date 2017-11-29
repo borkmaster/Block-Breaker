dbf7d36a52525b6d6cdb3dc75da3bf907db92edb
borkmaster <unluckymattress@gmail.com>
2017-11-29 13:20:52 -0600

    Blocks destroyed after appropriate # of hits

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	public int maxHits;
+	private int timesHit;
+	private LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		timesHit++;
+		if (timesHit >= maxHits) {
+			Destroy(gameObject);
+		}
+		// levelManager.LoadNextLevel();
+	}
+	
+}
